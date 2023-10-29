1ab48598fc1434242544404d9177be5f2db824c5
n48331 <unfolks@gmail.com>
2023-10-30 00:50:08 +0530

    image compression

--- foodspots/models.py
+++ foodspots/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 import os
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
 
 def upload_to(instance, filename):
     file_extension = filename.split('.')[-1]
@@ -17,14 +21,33 @@ class Category(models.Model):
 class FoodSpot(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to=upload_to,default='default/restarant.jpg')  # Configure MEDIA_ROOT and MEDIA_URL in your settings.
+    image = models.ImageField(upload_to=upload_to,default='default/restarant.jpg') 
     rating = models.DecimalField(max_digits=3, decimal_places=2)
     time = models.CharField(max_length=100)
-    location = models.CharField(max_length=255, unique=True)  # Store coordinates as a string, e.g., "latitude,longitude"
+    location = models.CharField(max_length=255, unique=True) 
     categories = models.ManyToManyField(Category)
     reel = models.URLField(blank=True, null=True, unique=True)
     def __str__(self):
         return self.name
+
+def compress_image(image, width=1200):
+    img = Image.open(image)
+    if img.mode == 'RGBA':
+        img = img.convert('RGB')
+    height = int((width / float(img.width)) * float(img.height))
+    img = img.resize((width, height), Image.Resampling.LANCZOS)
+    output = BytesIO()
+    img.save(output, format='JPEG', quality=95)
+    output.seek(0)
+    compressed_image = InMemoryUploadedFile(output, 'ImageField', f'{image.name.split(".")[0]}.jpg', 'image/jpeg',
+                                            output.getbuffer().nbytes, None)
+    return compressed_image
+
+
+@receiver(pre_save, sender=FoodSpot)
+def compress_foodspot_image(sender, instance, **kwargs):
+    if instance.image:
+        instance.image = compress_image(instance.image)
 
 @receiver(pre_save, sender=FoodSpot)
 def delete_old_image(sender, instance, **kwargs):
